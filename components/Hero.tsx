b726cdd6868a1b468dbac50087245ac107137d45
Stephen Leander <stephen.leander1@gmail.com>
2022-09-13 18:51:21 -0600

    Style Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,22 +3,55 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <div className="order-actions">
-      <Link href="/">Call Us</Link>
-      <div className="logo">
-        <Image
-          src="/../public/logos/eclipse.png"
-          alt="eclipse logo"
-          width={200}
-          height={200}
-        ></Image>
-        <div className="logo-text">
-          <h2>ECLIPSE</h2>
-          <h3>CANNABIS COMPANY</h3>
+    <section id="hero">
+      <Image
+        className="background-image"
+        src="/../public/images/blackmarble_2016_americas_composite.png"
+        layout="fill"
+        priority
+      ></Image>
+      <div className="content">
+        <div className="logo">
+          <Image
+            className="logo-image"
+            src="/../public/logos/eclipse.png"
+            alt="eclipse logo"
+            width={200}
+            height={200}
+            priority
+          ></Image>
+          <div className="logo-text">
+            <h3>ECLIPSE</h3>
+            <h4>CANNABIS COMPANY</h4>
+          </div>
+        </div>
+        <h2>VOTED BEST REC SELECTION</h2>
+        <div className="order-actions">
+          <Link href="/">
+            <a href="">
+              <Image
+                className="button-image"
+                src="/../public/images/stars.png"
+                layout="fill"
+                priority
+              ></Image>
+              <p>Call Us</p>
+            </a>
+          </Link>
+          <Link href="/">
+            <a href="">
+              <Image
+                className="button-image"
+                src="/../public/images/stars.png"
+                layout="fill"
+                priority
+              ></Image>
+              <p>Order Online</p>
+            </a>
+          </Link>
         </div>
       </div>
-      <Link href="/">Order Online</Link>
-    </div>
+    </section>
   );
 };
 
