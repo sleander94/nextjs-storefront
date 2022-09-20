58652fff81a128194f284cc0c8665d9c3d36209f
Stephen Leander <stephen.leander1@gmail.com>
2022-09-20 17:04:29 -0600

    Get age verification on load

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,24 +1,36 @@
 import Head from 'next/head';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import Footer from './Footer';
+import AgeVerifier from './AgeVerifier';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [isOver21, setIsOver21] = useState<boolean>(true);
+
+  useEffect(() => {
+    const ver = sessionStorage.getItem('eclipseOver21');
+    if (ver !== 'true') setIsOver21(false);
+  }, []);
   return (
     <>
-      <Head>
-        <title>Layouts Example</title>
-      </Head>
-      <Navbar />
-      <ParallaxProvider>
-        <main>{children}</main>
-      </ParallaxProvider>
-      <Footer />
+      {!isOver21 && <AgeVerifier setIsOver21={setIsOver21} />}
+      {isOver21 && (
+        <>
+          <Head>
+            <title>Layouts Example</title>
+          </Head>
+          <Navbar />
+          <ParallaxProvider>
+            <main>{children}</main>
+          </ParallaxProvider>
+          <Footer />
+        </>
+      )}
     </>
   );
 };
