a286c87c57e3590a8c11a0b57640c50bf6741171
Stephen Leander <stephen.leander1@gmail.com>
2022-09-20 17:04:58 -0600

    Add address & phone links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,57 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <section id="footer">
       <Image
         className="background-image"
-        src="/../public/images/purple-space.jpg"
+        src="/images/purple-space.jpg"
         alt="outer space background"
         layout="fill"
       ></Image>
       <div className="content">
-        <h2></h2>
+        <a className="phone" href="">
+          <Image
+            style={{
+              filter:
+                'invert(30%) sepia(42%) saturate(3555%) hue-rotate(313deg) brightness(92%) contrast(101%)',
+            }}
+            src="/phone.svg"
+            alt="phone"
+            width={24}
+            height={24}
+          />
+          <p>(720) 420-0782</p>
+        </a>
         <div className="title">
           <Image
-            src="/../public/logos/eclipse.png"
+            src="/logos/eclipse.png"
             alt="eclipse logo"
-            width={64}
-            height={64}
+            width={42}
+            height={42}
           />
           <h2>ECLIPSE CANNABIS COMPANY 2022</h2>
         </div>
+        <a
+          className="address"
+          href="https://www.google.com/maps/@40.0258735,-105.2870899,17z"
+        >
+          <Image
+            style={{
+              filter:
+                'invert(30%) sepia(42%) saturate(3555%) hue-rotate(313deg) brightness(92%) contrast(101%)',
+            }}
+            src="/location.svg"
+            alt="location marker"
+            width={24}
+            height={24}
+          />
+          <p>
+            933 ALPINE AVE, SUITE 2<br></br>
+            BOULDER, CO 80304
+          </p>
+        </a>
       </div>
     </section>
   );
