fe40195f2b8ec526d40c51f6421c9663a7adb08d
Stephen Leander <stephen.leander1@gmail.com>
2022-09-22 17:49:13 -0600

    Render form without pressing join button, edit layout

--- components/LoyaltySignup.tsx
+++ components/LoyaltySignup.tsx
@@ -1,51 +1,147 @@
 import Image from 'next/image';
-import { useState } from 'react';
-import SignupForm from './SignupForm';
+import { useState, FormEvent, ChangeEvent } from 'react';
 
 const LoyaltySignup = () => {
-  const [signupForm, setSignupForm] = useState<boolean>(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    birthday: '',
+  });
 
+  const [isMedical, setIsMedical] = useState<boolean>(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData((data) => ({ ...data, [name]: value }));
+  };
+
+  const [nameErr, setNameErr] = useState<string>();
+  const [emailErr, setEmailErr] = useState<string>();
+  const [phoneErr, setPhoneErr] = useState<string>();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      console.log(formData);
+      console.log(isMedical);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
-    <>
-      <section id="loyalty-signup">
-        <Image
-          className="background-image"
-          src="/images/stars.png"
-          layout="fill"
-        ></Image>
-        <div className="content">
+    <section id="loyalty-signup">
+      <Image
+        className="background-image"
+        src="/images/stars.png"
+        layout="fill"
+      ></Image>
+      <div className="content">
+        <div className="call-to-action">
           <h2>BE THE FIRST TO GET EXCLUSIVE DEALS!</h2>
-          <div className="form-actions">
-            <div className="rocket">
-              <Image
-                style={{
-                  filter:
-                    'invert(30%) sepia(42%) saturate(3555%) hue-rotate(313deg) brightness(92%) contrast(101%)',
-                }}
-                src="/rocket.svg"
-                alt="rocket ship blasting off"
-                width={100}
-                height={100}
-              />
-            </div>
-            <div className="call-to-action">
-              <h3>Sign up for our text & loyalty program today.</h3>
-              <button onClick={() => setSignupForm(true)}>
-                <div className="background-gradient">
-                  <p>Join Now!</p>
-                </div>
-                <Image
-                  className="button-image"
-                  src="/images/stars.png"
-                  layout="fill"
-                ></Image>
-              </button>
-            </div>
-          </div>
+          <h3>Sign up for our text & loyalty program today.</h3>
         </div>
-      </section>
-      {signupForm && <SignupForm setSignupForm={setSignupForm} />}
-    </>
+        <form id="signup-form" action="" onSubmit={handleSubmit} noValidate>
+          <div className="form-field">
+            <label htmlFor="name">Name*</label>
+            <input
+              id="name"
+              aria-label="name"
+              type="text"
+              name="name"
+              required
+              onChange={(e) => handleChange(e)}
+              onBlur={(e) => {
+                e.target.className = 'touched';
+                formData.name === ''
+                  ? setNameErr('Name is required.')
+                  : setNameErr(undefined);
+              }}
+            />
+            {nameErr && <p className="error">{nameErr}</p>}
+          </div>
+          <div className="form-field">
+            <label htmlFor="phone">Phone*</label>
+            <input
+              id="phone"
+              aria-label="phone"
+              type="tel"
+              name="phone"
+              required
+              onChange={(e) => handleChange(e)}
+              onBlur={(e) => {
+                e.target.className = 'touched';
+                formData.phone.length === 0
+                  ? setPhoneErr('Phone number is required.')
+                  : formData.phone.match(/[a-zA-Z]/gi)
+                  ? setPhoneErr('Invalid phone number.')
+                  : setPhoneErr(undefined);
+              }}
+            />
+            {phoneErr && <p className="error">{phoneErr}</p>}
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              aria-label="email"
+              type="email"
+              name="email"
+              onChange={(e) => handleChange(e)}
+              onBlur={(e) => {
+                e.target.className = 'touched';
+                formData.email.length > 0 &&
+                !formData.email.match(
+                  /([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
+                )
+                  ? setEmailErr('Invalid email.')
+                  : setEmailErr(undefined);
+              }}
+            />
+            {emailErr && <p className="error">{emailErr}</p>}
+          </div>
+          <div className="form-field">
+            <label htmlFor="phone">Birthday</label>
+            <input
+              id="birthday"
+              aria-label="birthday"
+              type="date"
+              name="birthday"
+              required
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              onBlur={(e) => {
+                e.target.className = 'touched';
+              }}
+            />
+          </div>
+
+          <div className="form-field-check">
+            <input
+              type="checkbox"
+              name="isMedical"
+              onChange={() => setIsMedical(!isMedical)}
+            />
+            <label htmlFor="textAlert">I am a medical patient.</label>
+          </div>
+
+          <button>
+            <div className="background-gradient">
+              <p>Join Now!</p>
+            </div>
+            <div className="image-container">
+              <Image
+                className="button-image"
+                src="/images/stars.png"
+                layout="fill"
+              ></Image>
+            </div>
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
