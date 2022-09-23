2a2573814dfc31f6a59429db198ef120ca3d3c4c
Stephen Leander <stephen.leander1@gmail.com>
2022-09-23 17:12:06 -0600

    Escape characters, add rel to <a>

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -15,10 +15,10 @@ const Testimonials = () => {
           <div className="border">
             <div className="review">
               <p>
-                "Rylie, kyle, caleb are the best in town. If you’re not getting
-                concentrates and flower from here you are truly doing yourself a
-                disservice. Check them out. I’ve lived in Boulder 13 years this
-                place is my go to.
+                &quot;Rylie, kyle, caleb are the best in town. If you&apos;re
+                not getting concentrates and flower from here you are truly
+                doing yourself a disservice. Check them out. I&apos;ve lived in
+                Boulder 13 years this place is my go to.&quot;
               </p>
               <p className="sig">
                 <span>-</span> Ronald W.
@@ -28,10 +28,11 @@ const Testimonials = () => {
           <div className="border">
             <div className="review">
               <p>
-                "Always a pleasure coming into eclipse! The staff team never
-                fails to provide excellent customer service and knowledge on all
-                products. I’ve been coming here since I’ve turned 21 and don’t
-                plan on making any other dispensary my go-to."{' '}
+                &quot;Always a pleasure coming into eclipse! The staff team
+                never fails to provide excellent customer service and knowledge
+                on all products. I&apos;ve been coming here since I&apos;ve
+                turned 21 and don&apos;t plan on making any other dispensary my
+                go-to.&quot;
               </p>
               <p className="sig">
                 <span>-</span> Jeremy S.
@@ -41,10 +42,10 @@ const Testimonials = () => {
           <div className="border">
             <div className="review">
               <p>
-                "Eclipse is, without a doubt in my mind, the best spot in
+                &quot;Eclipse is, without a doubt in my mind, the best spot in
                 Boulder! They have the best selection of flower and concentrates
                 at the best prices, hands down. After discovering this gem, I
-                refuse to go to any other dispo in Boulder."
+                refuse to go to any other dispo in Boulder.&quot;
               </p>
               <p className="sig">
                 <span>-</span> Matthew W.
@@ -55,6 +56,7 @@ const Testimonials = () => {
         <a
           href="https://www.google.com/maps/place/Eclipse+Cannabis+Company/@40.0258889,-105.287186,17z/data=!4m7!3m6!1s0x0:0x521980b3bd00abdd!8m2!3d40.0258735!4d-105.2849012!9m1!1b1"
           target="_blank"
+          rel="noreferrer"
           className="reviews-link"
         >
           <div className="background-gradient">
