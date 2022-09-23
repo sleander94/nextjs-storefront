de885599344b9ca8212883107aa032ab358453b8
Stephen Leander <stephen.leander1@gmail.com>
2022-09-23 12:58:22 -0600

    Create page to display brand links & logos

---
+++ pages/brands.tsx
@@ -0,0 +1,14 @@
+import type { ReactElement } from 'react';
+import Layout from '../components/Layout';
+import type { NextPageWithLayout } from './_app';
+import Brands from '../components/Brands';
+
+const BrandsPage: NextPageWithLayout = () => {
+  return <Brands />;
+};
+
+BrandsPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default BrandsPage;
