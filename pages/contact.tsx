7888142c2a19c35f870d206bd526825788349e4c
Stephen Leander <stephen.leander1@gmail.com>
2022-09-23 13:02:25 -0600

    Create page to display contact information

---
+++ pages/contact.tsx
@@ -0,0 +1,14 @@
+import type { ReactElement } from 'react';
+import Layout from '../components/Layout';
+import type { NextPageWithLayout } from './_app';
+import Contact from '../components/Contact';
+
+const ContactPage: NextPageWithLayout = () => {
+  return <Contact />;
+};
+
+ContactPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default ContactPage;
